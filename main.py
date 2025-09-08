4ed5b30673db2aee9e5354ab78a92f9f68cf5531
Andrew Nguyen <andrewqqn@gmail.com>
2025-09-08 13:52:39 -0700

    cros allow frontend origin

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = FastAPI()
 # Add this after creating your FastAPI app
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Your frontend URLs
+    allow_origins=["http://localhost:5173", "http://165.22.155.118:5173"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
